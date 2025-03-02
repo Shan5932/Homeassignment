f0ff1aa543c73c88685dfae1b48f0cbb4e4fa38d
Shanmugam <89644909+Shan5932@users.noreply.github.com>
2025-03-02 18:58:16 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package Homeassignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float versionoftype;
+		String developer;
+		boolean isbeta;
+		int releaseyear;
+		char shortcutkey;
+		
+		System.out.println(91.0);
+		System.out.println("google");
+		System.out.println(false);
+		System.out.println(2008);
+		System.out.println(false);
+		
+		
+	}
+
+}
---
+++ Firefox.java
@@ -0,0 +1,22 @@
+package Homeassignment;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float broswerversion;
+		String browsername;
+		boolean isvisbile;
+		int releaseyear;
+		char browserlogo;
+		
+		System.out.println("100.2");
+		System.out.println("firefox");
+		System.out.println("true");
+		System.out.println(1998);
+		System.out.println('f');
+		
+	}
+
+}
